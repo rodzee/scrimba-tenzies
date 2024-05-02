0bde63336509611ff9d610a16d818e11b2a28f41
rodzee <souzafeb@gmail.com>
2024-05-02 12:30:55 -0400

    fifth challenge completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,31 @@ import "./App.css";
 import Die from "./Die";
 
 function App() {
-  const [allDice, setAllDice] = useState(() => {
+  const [allDice, setAllDice] = useState(allNewDice());
+
+  function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
       newDice.push(Math.ceil(Math.random() * 6));
     }
     return newDice;
-  });
+  }
 
   const diceElement = allDice.map((dice) => <Die value={dice} />);
 
+  function handleRoll() {
+    setAllDice(allNewDice());
+  }
+
   return (
     <main>
+      <header>Tenzies</header>
+      <aside>
+        Roll until all dice are the same.{" "}
+        <p>Click each die to freeze it at its current value between rolls.</p>
+      </aside>
       <div className="container">{diceElement}</div>
+      <button onClick={handleRoll}>Roll</button>
     </main>
   );
 }
